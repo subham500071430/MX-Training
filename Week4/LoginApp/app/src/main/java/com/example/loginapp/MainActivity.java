7aad1b7d13c5f71d16fa280aec0269cd181faa45
Subham Kumar <subham10022000@gmail.com>
2022-04-10 19:55:32 +0530

    t2s

--- Week4/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
+++ Week4/LoginApp/app/src/main/java/com/example/loginapp/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.loginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText e1,e2;
+    EditText e1, e2;
     Button b1;
-    String s1,s2;
+    String s1, s2;
+    TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,19 +26,30 @@ public class MainActivity extends AppCompatActivity {
         e2 = findViewById(R.id.editText2);
         b1 = findViewById(R.id.button);
 
-        b1.setOnClickListener(new View.OnClickListener(){
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
 
+                // if No error is found then only it will run
+                if (i != TextToSpeech.ERROR) {
+                    // To Choose language of speech
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 s1 = e1.getText().toString();
                 s2 = e2.getText().toString();
 
                 if(s1.contentEquals("Subham")&&s2.contentEquals("subham7")){
-                    Toast.makeText(MainActivity.this,"Login Succesful",Toast.LENGTH_SHORT).show();
+                    textToSpeech.speak("Login Successful", TextToSpeech.QUEUE_FLUSH, null);
                 }
                 else
                 {
-                    Toast.makeText(MainActivity.this,"Login Unsuccesful",Toast.LENGTH_SHORT).show();
+                    textToSpeech.speak("Login Not Successful", TextToSpeech.QUEUE_FLUSH, null);
                 }
             }
         });
